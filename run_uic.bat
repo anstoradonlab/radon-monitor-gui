a846147ab3f1e2296c790a384354f5a187cefaf1
Alan Griffiths <Alan.Griffiths@ansto.gov.au>
2022-09-15 15:11:15 +1000

    Fix error in UIC driver script

--- run_uic.bat
+++ run_uic.bat
@@ -5,5 +5,5 @@ pyuic5 ui/data_view.ui > src/main/python/ui_data_view.py
 pyuic5 ui/c_and_b.ui > src/main/python/ui_c_and_b.py
 pyuic5 ui/system_information.ui > src/main/python/ui_system_information.py
 pyuic5 ui/sensitivity_sweep.ui > src/main/python/ui_sensitivity_sweep.py
-pyuic5 ui/cal_bg_start_time_widget.ui > src/main/python/cal_bg_start_time_widget.py
+pyuic5 ui/cal_bg_start_time_widget.ui > src/main/python/ui_cal_bg_start_time_widget.py
 
