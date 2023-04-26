791ba76ba3096cf6debe8305bb9c7567451f8432
Alan Griffiths <Alan.Griffiths@ansto.gov.au>
2023-04-26 14:56:35 +1000

    Handle None when plotting data (part 2)

--- src/main/python/data_plotter.py
+++ src/main/python/data_plotter.py
@@ -80,7 +80,8 @@ class DataPlotter(object):
         po["idx"] = idx
         try:
             x = data[xvar]
-            y = data[yvar]
+            # conversion into dtype=float converts None values into nan
+            y = np.array(data[yvar], dtype=float)
             legend_data = data[huevar]
         except Exception as e:
             _logger.error(f"Encounted error while generating plot: {e}")
